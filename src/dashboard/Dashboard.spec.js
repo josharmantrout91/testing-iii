e64415ce754c1d4e14b67d01a33d6da560a56b53
josharmantrout91 <josh.armantrout@gmail.com>
2019-03-13 17:48:20 -0400

    Basic tests passing for Display

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,14 +1,12 @@
 // Test away
 
 import React from "react";
-import ReactDom from "react-dom";
-import renderer from "react-test-renderer";
 
 import Dashboard from "./Dashboard";
 import { render } from "react-testing-library";
 
-describe("Dashboard Component", () => {
+describe("<Dashboard />", () => {
   it("renders without crashing", () => {
-    render(<Dashboard />);
+    const { getByText } = render(<Dashboard />);
   });
 });
