80840cb48301e6be93b5a0dbfbb632a7ab66e370
josharmantrout91 <josh.armantrout@gmail.com>
2019-03-13 16:58:27 -0400

    Sanity check test passes on Dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,14 @@
 // Test away
+
+import React from "react";
+import ReactDom from "react-dom";
+import renderer from "react-test-renderer";
+
+import Dashboard from "./Dashboard";
+import { render } from "react-testing-library";
+
+describe("Dashboard Component", () => {
+  it("renders without crashing", () => {
+    render(<Dashboard />);
+  });
+});
